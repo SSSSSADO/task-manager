575201d2c7741cefb86d7a48e8264414e5c36d0c
SSSSSADO <grushkovik.danil@gmail.com>
2026-04-03 12:32:24 +0300

    Add models Tag & Task

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField()
+    completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True,
+        null=True,
+        related_name="tasks",
+    )
+
+    class Meta:
+        ordering = ["-completed", "-datetime"]
+
+    def __str__(self):
+        return f"{self.id}: {self.content} completed: {self.completed}"
