56f707f03dd556605f88f9b211b60e7829ef8a50
SSSSSADO <grushkovik.danil@gmail.com>
2026-04-03 13:09:44 +0300

    Add path & page for task_list ~ home page

--- manager/urls.py
+++ manager/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from manager import views
+
+
 app_name = "manager"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.TaskListView.as_view(), name="task_list"),
+]
